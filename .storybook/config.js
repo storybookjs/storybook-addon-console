d9eff6d30b9e873c882d31434da84b3677eac73b
Oleg Proskurin <regx@usul.su>
2018-10-24 21:10:30 +0300

    update story

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,12 +1,17 @@
 import React from 'react';
 import { configure, addDecorator } from '@storybook/react';
+import { setOptions } from '@storybook/addon-options';
 import { setConsoleOptions } from '../src';
+
+setOptions({
+  name: 'Addon Console',
+  url: 'https://github.com/storybooks/storybook-addon-console',
+});
 
 const panelExclude = setConsoleOptions({}).panelExclude;
 setConsoleOptions({
   panelExclude: [...panelExclude, /deprecated/],
 });
-
 
 function loadStories() {
   require('../stories/index.stories.js');
--- stories/index.stories.js
+++ stories/index.stories.js
@@ -4,9 +4,9 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 import { storiesOf } from '@storybook/react';
-import { linkTo } from '@storybook/addon-links';
 
-import { Button, Welcome } from '@storybook/react/demo';
+import { Button } from '@storybook/react/demo';
+import welcome from '../docs/welcome';
 import { withConsole } from '../src';
 
 const T = {
@@ -20,45 +20,46 @@ class Badge extends React.Component {
   }
   render() {
     console.log('Badge render');
-    return (
-      <div>
-        {this.props.info}
-      </div>
-    );
+    return <div>{this.props.info}</div>;
   }
 }
 Badge.propTypes = {
   info: PropTypes.number.isRequired,
 };
 
-storiesOf('Welcome', module).add('to Storybook', () => <Welcome showApp={linkTo('Button')} />);
+storiesOf('Welcome', module).add('to Storybook Addon Console', () => welcome);
 
 storiesOf('Button', module)
+  // simple console.log may be located anywhere deep in the child component
   .add('with Log', () => <Button onClick={() => console.log('Data:', 1, 3, 4)}>Log Button</Button>)
-  .add('with Warning', () =>
+  // simple console.warn may be located anywhere deep in the child component
+  .add('with Warning', () => (
     <Button onClick={() => console.warn('Data:', 1, 3, 4)}>Warn Button</Button>
-  )
-  .add('with Error', () =>
+  ))
+  // simple console.error may be located anywhere deep in the child component
+  .add('with Error', () => (
     <Button onClick={() => console.error('Test Error')}>Error Button</Button>
-  )
-  .add('with Uncatched Error', () =>
-    <Button onClick={() => console.log('Data:', T.buu.foo)}>Throw Button</Button>
-  )
+  ))
+  // any unhandled error from anywhere deep in the child component
+  .add('with Uncatched Error', () => (
+    <Button onClick={() => console.log('Data:', null.buu.foo)}>Throw Button</Button>
+  ))
+  // logs and warnings displayed as soon as a child component did Mount
   .add('with wrong PropType', () => <Badge info="Component with React Warning" />);
 
 const addConsole = (storyFn, context) => withConsole()(storyFn)(context);
 storiesOf('withConsole', module)
-  // .addDecorator((storyFn, context) => withConsole()(storyFn)(context))
+  // all messages prefixed with story name
   .addDecorator(addConsole)
   .add('with Log', () => <Button onClick={() => console.log('Data:', 1, 3, 4)}>Log Button</Button>)
-  .add('with Warning', () =>
+  .add('with Warning', () => (
     <Button onClick={() => console.warn('Data:', 1, 3, 4)}>Warn Button</Button>
-  )
-  .add('with Error', () =>
+  ))
+  .add('with Error', () => (
     <Button onClick={() => console.error('Test Error')}>Error Button</Button>
-  )
-  .add('with Uncatched Error', () =>
+  ))
+  .add('with Uncatched Error', () => (
     <Button onClick={() => console.log('Data:', T.buu.foo)}>Throw Button</Button>
-  )
+  ))
   .add('with log in constructor', () => <Badge info={42} />)
   .add('with wrong PropType', () => <Badge info="Component with React Warning" />);
