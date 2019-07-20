3f746cd173029eba08d7b044a9226f2b6a33b757
Oleg Proskurin <regx@usul.su>
2019-07-21 01:30:43 +0300

    Update theme

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,27 +1,30 @@
 import React from 'react';
-import { configure, addDecorator } from '@storybook/react';
-import { withOptions } from '@storybook/addon-options';
+import { configure, addDecorator, addParameters } from '@storybook/react';
 import { create } from '@storybook/theming';
 
 import brandImage from '../docs/logo.svg';
 import { setConsoleOptions } from '../src';
 
 const theme = create({
-  base: 'light',
+  base: 'dark',
 
   colorPrimary: 'red',
   colorSecondary: '#58487b',
   brandTitle: 'Addon Console',
   brandUrl: 'https://github.com/storybookjs/storybook-addon-console',
   brandImage,
+  appContentBg: '#251f33',
+  appBg: '#6d608a',
+  barBg: '#3b3152',
+  barSelectedColor: 'white',
+  background: { content: 'red' },
+
+  addonActionsTheme: {
+    // ...chromeLight,
+    // BASE_FONT_FAMILY: typography.fonts.mono,
+    BASE_BACKGROUND_COLOR: 'black',
+  },
 });
-
-addDecorator(
-  withOptions({
-    theme,
-  })
-);
-
 
 const panelExclude = setConsoleOptions({}).panelExclude;
 setConsoleOptions({
@@ -31,5 +34,12 @@ setConsoleOptions({
 function loadStories() {
   require('../stories/index.stories.js');
 }
+
+addParameters({
+  options: {
+    theme,
+  },
+  backgrounds: [{ name: 'default', value: '#e7e2f3', default: true }],
+});
 
 configure(loadStories, module);
